9e86d0b8058aed467531bb09285f46f8de76e516
dragon-fire-fly <ek93@hotmail.co.uk>
2022-12-22 16:58:13 +0100

    Finish django blog

--- blog/urls.py
+++ blog/urls.py
@@ -1,4 +1,8 @@
 from django.urls import path, include
 from . import views
 
-urlpatterns = [path("", views.PostList.as_view(), name="home")]
+urlpatterns = [
+    path("", views.PostList.as_view(), name="home"),
+    path("<slug:slug>/", views.PostDetail.as_view(), name="post_detail"),
+    path("like/<slug:slug>/", views.PostLike.as_view(), name="post_like"),
+]
